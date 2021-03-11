46685430527ee7f9a0a47693a09704e71c5181de
Braxton McClellan <thebraxmc@gmail.com>
2021-03-10 23:46:27 -0700

    some dice logic added, basic functionality

--- script.js
+++ script.js
@@ -24,9 +24,15 @@ const currentP1 = document.getElementById('currentP1');
 const currentP2 = document.getElementById('currentP2');
 const totalP1 = document.getElementById('totalP1');
 const totalP2 = document.getElementById('totalP2');
+const diceEl1 = document.getElementById('dice1');
+const diceEl2 = document.getElementById('dice2');
+const diceEl3 = document.getElementById('dice3');
+const diceEl4 = document.getElementById('dice4');
+const diceEl5 = document.getElementById('dice5');
+const diceEl6 = document.getElementById('dice6');
 
+const resetBtn = document.querySelector('.reset');
 const btnRoll = document.querySelector('.rollBtn');
-const diceEl1 = document.querySelector('.dice1')
 
 let scores, currentScore, currentPlayer, playing;
 
@@ -60,10 +66,31 @@ btnRoll.addEventListener('click', function() {
     const dice4 = diceRoll();
     const dice5 = diceRoll();
     const dice6 = diceRoll();
-    console.log(dice1, dice2, dice3, dice4, dice5, dice6);
+    let roll = [dice1, dice2, dice3, dice4, dice5, dice6]
+    // console.log(dice1, dice2, dice3, dice4, dice5, dice6);
+    console.log(roll);
+    if(roll.includes(1) || roll.includes(5)) {
+      for(let i = 0; i < roll.length; i++) {
+        if(roll[i] == 1) {
+          currentScore += 100
+          currentP1.textContent = currentScore;
+        } else if(roll[i] == 5) {
+          currentScore += 50
+          currentP1.textContent = currentScore;
+        }
+      }
+    } else {
+      document.getElementById('begin').textContent = 'ZONKADILLY!'
+    }
     // display dice
-    // diceEl1.src = `dice-${num}.png`
+    diceEl1.src = `./images/dice-${dice1}.png`;
+    diceEl2.src = `./images/dice-${dice2}.png`;
+    diceEl3.src = `./images/dice-${dice3}.png`;
+    diceEl4.src = `./images/dice-${dice4}.png`;
+    diceEl5.src = `./images/dice-${dice5}.png`;
+    diceEl6.src = `./images/dice-${dice6}.png`;
   }
 })
 
-
+// reset to new game
+resetBtn.addEventListener('click', init);
